9bd1f05de66ba101c9f9b6ac9fbbd9930cfd8946
Nicolas Picotto <nicopicotto@hotmail.com>
2025-09-07 16:06:25 -0300

    cambio en workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,22 @@
 import tailwindcss from "@tailwindcss/vite";
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	
-	vite: {
-		plugins: [tailwindcss()],
-	},
+   // Especifica tu dominio
+   site: "https://lavaugc.com", // Cambia esto por tu dominio real
+
+   // Asegúrate de que sea output estático
+   output: "static",
+
+   // Especifica la carpeta de salida
+   outDir: "./dist",
+
+   // Base path (si tu sitio está en la raíz, déjalo como '/')
+   base: "/",
+
+   vite: {
+      plugins: [tailwindcss()],
+   },
 });
