cd0c30c761ac82dcf8a999b9b41b5fece0785b33
Nicolas Picotto <nicopicotto@hotmail.com>
2025-12-20 14:46:52 -0300

    lavalab init

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,15 +16,6 @@ export default defineConfig({
    // Base path (si tu sitio está en la raíz, déjalo como '/')
    base: "/",
 
-   // Configuración de i18n
-   i18n: {
-      defaultLocale: "es",
-      locales: ["es", "en"],
-      routing: {
-         prefixDefaultLocale: false, // español sin prefijo (/), inglés con prefijo (/en/)
-      },
-   },
-
    vite: {
       plugins: [tailwindcss()],
    },
